45e60c471967f4c60f2e8334d7d3e4ad473bf492
Camila Kadi <camilakadi@hotmail.com>
2024-02-25 22:08:42 -0300

    feat: Adiciona página de favoritos
    - Implementa a página de favoritos para exibir os itens marcados como favoritos pelo usuário

---
+++ src/pages/favoritos.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/Header/Header";
+import Repository from "@/components/Repository/Repository";
+
+const FavoritesPage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container mx-auto px-4 lg:px-0 pt-6 max-w-[896px]">
+          <h1 className="text-primary-color mb-6 text-center">
+            Meus favoritos
+          </h1>
+          <Repository />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default FavoritesPage;
